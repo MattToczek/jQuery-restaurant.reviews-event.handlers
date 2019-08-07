7161460c30f49027936a630d2fdd4c739f86b200
Matt. Toczek <toczek.matt@gmail.com>
2019-08-07 12:40:23 +0100

    adding new li from input

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 // 1. Hide/show the text inside the #helpText span element when the user's mouse
 //passes over the text "How are these ranked?" (Hint: use the jQuery toggle() method)
 $('#helpText').on('mouseover', function(event){
-  console.log($(this))
   $(this).children(0).toggle('display(auto)');
 })
 
 // 2. Append a new restaurant to the restaurant list when the user enters a
 //value into the #newRestaurant input and clicks on the #addNew button.
-
+$('#addNew').on('click', function(event){
+  let newLi = '<li>' + $('#newRestaurantInput').val() + '</li>';
+  console.log($('#newRestaurantInput.value'));
+  $('ul').append(newLi);
+// $('#helpText').next().append($('#newRestaurantInput').val());
+})
 
   // BONUS: Before appending the new restaurant to the list, use string
   //concatentation to make sure the new restaurant name is surrounded by an opening and closing <li> tag.
+
 
 // 3. Add the class "strike" to the restaurant name each time it's double
 //clicked. Use event delegation on the ul element to listen for a click event
