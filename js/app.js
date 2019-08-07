be818f399489359ffbaad6bb88423b4e93e6644c
Matt. Toczek <toczek.matt@gmail.com>
2019-08-07 11:28:53 +0100

    adding mouseover display of extra info

--- js/app.js
+++ js/app.js
@@ -1,29 +1,21 @@
-// 1. Show the hidden restaurant review, or use an animation method to reveal it.
-$('.review').show();
-// 2. Set the title of the hidden restaurant review to a different name -- whatever you'd like.
-$('.review h3').text('Dems Dawgs');
-// 3. Change the text of the hidden restaurant review. The review should contain HTML, such as a strong tag or a span element.
-$('.review p').html('Deez are de <strong>Best</strong> Dawgs I ever did ates');
-// 4. Change the name of the app. Change *only* the text inside the span element within the h1.
-$('h1 span').text("Matty-Pops'  ");
-// 5. Create an animation for the paragraph with the id of "flashMessage." Chain an animation that
-$('#flashMessage')
+// 1. Hide/show the text inside the #helpText span element when the user's mouse
+//passes over the text "How are these ranked?" (Hint: use the jQuery toggle() method)
+$('#helpText').on('mouseover', function(event){
+  console.log($(this))
+  $(this).children(0).toggle('display(auto)');
+})
 
-  .slideDown(1000)
-  // slides the message down for 1 second
-  .delay(2000)
-  // delays the message for 2 seconds
-  .slideUp(1000)
-  // slides the message up for 1 second
+// 2. Append a new restaurant to the restaurant list when the user enters a
+//value into the #newRestaurant input and clicks on the #addNew button.
 
-// 6. Inside the click handler, create a working restaurant review form.
-$('#submitBtn').click(function(){
-  let resName = $('#restaurantNameInput').val();
-  let resReview = $('#restaurantReviewInput').val();
-  //  a. Use the val() method to get what's been entered into the restaurant name and restaurant review form input fields. Save both values to variables.
 
-  $('#newRestaurant h3').text(resName);
-  //  b. Insert the restaurant name value into the #newRestaurant h3 tag
-  $('#newRestaurant p').text(resReview);
-  //  c. Insert the restaurant review value into the #newRestaurant paragraph tag
-});
+  // BONUS: Before appending the new restaurant to the list, use string
+  //concatentation to make sure the new restaurant name is surrounded by an opening and closing <li> tag.
+
+// 3. Add the class "strike" to the restaurant name each time it's double
+//clicked. Use event delegation on the ul element to listen for a click event
+//on the ul's list items
+
+//BONUS: Refactor challenge 3 using the "this" keyword.
+//BONUS: Look up a jQuery method that will let you toggle the .strike class
+//on and off.
